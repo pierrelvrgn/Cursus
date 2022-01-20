3c8e4c0a1edd891b7a53b922ea00874a8f8ab4a4
Pierre <pierre.lvrgn@gmail.com>
2022-01-20 10:46:26 +0100

    main

---
+++ LIBFT/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 12:05:39 by plavergn          #+#    #+#             */
+/*   Updated: 2021/11/05 13:33:51 by plavergn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		i;
+	int		a;
+
+	i = 0;
+	a = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[a])
+	{
+		dest[i] = s2[a];
+		i++;
+		a++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
